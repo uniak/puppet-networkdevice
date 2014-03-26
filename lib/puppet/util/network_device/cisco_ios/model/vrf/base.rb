13225609d73bba32e17a62bef966b7257326fa07
David Schmitt <david@dasz.at>
2014-03-26 15:23:14 +0100

    model/vrf/base: extract vrf_prompt

--- lib/puppet/util/network_device/cisco_ios/model/vrf/base.rb
+++ lib/puppet/util/network_device/cisco_ios/model/vrf/base.rb
@@ -4,6 +4,8 @@ require 'puppet/util/network_device/cisco_ios/model/vrf'
 module Puppet::Util::NetworkDevice::Cisco_ios::Model::Vrf::Base
   def self.register(base)
     vrf_scope = /\n(ip vrf (\w+)\s+([^!]*))\n!/m
+    vrf_prompt = /\(config-vrf\)#\s?\z/n
+    
     base.register_scoped :ensure, vrf_scope do
       match do |txt|
         unless txt.nil?
@@ -21,20 +23,20 @@ module Puppet::Util::NetworkDevice::Cisco_ios::Model::Vrf::Base
       match /^\s*description (.*?)\s*$/
       cmd 'sh run'
       add do |transport, value|
-        transport.command("description #{value}", :prompt => /\(config-vrf\)#\s?\z/n)
+        transport.command("description #{value}", :prompt => vrf_prompt)
       end
       remove do |transport, value|
-        transport.command("no description", :prompt => /\(config-vrf\)#\s?\z/n)
+        transport.command("no description", :prompt => vrf_prompt)
       end
     end
     base.register_scoped :rd, vrf_scope do
       match /^\s*rd (.*?)\s*$/
       cmd 'sh run'
       add do |transport, value|
-        transport.command("rd #{value}", :prompt => /\(config-vrf\)#\s?\z/n)
+        transport.command("rd #{value}", :prompt => vrf_prompt)
       end
       remove do |transport, value|
-        transport.command("no rd #{value}", :prompt => /\(config-vrf\)#\s?\z/n)
+        transport.command("no rd #{value}", :prompt => vrf_prompt)
       end
     end
   end
